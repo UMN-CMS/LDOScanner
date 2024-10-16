11c87f55b20fa47be710f71af59aba86d926841c
kapsiak <kapsi002@umn.edu>
2024-10-16 11:06:00 -0500

    More

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,27 +2,51 @@
   import { BrowserMultiFormatReader, BarcodeFormat } from '@zxing/library';
   const codeReader = new BrowserMultiFormatReader()
 
-  let selected_device;
+  let selected_device_id;
   console.log('ZXing code reader initialized')
-
+  
   async function getDevices(){
     let devices = await codeReader.listVideoInputDevices()
-    console.log(devices)
     return devices
   }
 
-  
+  let promise  = getDevices()
+
+
+  let decode_promise  = null;
+
+  async function decode(device_id){
+    decode_promise = new Promise();
+    let value = await codeReader.decodeFromVideoDevice(device_id, 'video',
+                                                       (result, err) => {
+                                                         if (result) {
+                                                           console.log(result)
+                                                           document.getElementById('result').textContent = result.text
+                                                         }
+                                                         if (err && !(err instanceof ZXing.NotFoundException)) {
+                                                           console.error(err)
+                                                           document.getElementById('result').textContent = err
+                                                         }
+    })
+  )
+    return value
+  }
+
 </script>
 <main>
 
-  <select bind:value={selected_device}>
-    {#await getDevices() }
-		<option value={null}>
-          Loading Available Video Devices...
-		</option>
+  <div>
+    <video id="video" width="300" height="200" style="border: 1px solid gray"></video>
+  </div>
+
+  <select bind:value={selected_device_id}>
+    {#await promise }
+	  <option value={null}>
+        Loading Available Video Devices...
+	  </option>
     {:then devices}
 	  {#each devices as device}
-		<option value={device}>
+		<option value={device.id}>
 		  {device.label}
 		</option>
 	  {/each}
@@ -30,6 +54,16 @@
 	  System error: {someError.message}.
     {/await}
   </select>
+
+  <button on:click="{decode(selected_device_id)}"> Start Decode</button>
+
+  {#await decode_promise }
+    Decoding
+  {:then value}
+    {value}
+  {:catch someError}
+	System error: {someError.message}.
+  {/await}
 </main>
 
 <style>
