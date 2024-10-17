7bd4619c97dadecb12e97ee2bd0659f6b30842b9
Charlie Kapsiak <kapsi002@umn.edu>
2024-10-17 15:45:26 -0500

    Change spacing

--- src/routes/BarcodeBox.svelte
+++ src/routes/BarcodeBox.svelte
@@ -17,16 +17,16 @@
  $: is_valid = (regex === null) ||  regex.test(barcode);
 </script>
 
-<div class="box">
+<div class="box p-6">
     <div class="columns is-vcentered">
-	    <div class="column is-one-third has-text-centered">
-		    <span class="is-size-6 is-text-centered"> {component_type} </span>
+	    <div class="mt-1 mb-1 pt-1 pb-1 block column is-one-third has-text-centered">
+		    <span class="is-size-5 is-text-centered"> {component_type} </span>
 	    </div>
-	    <div class="column is-one-third has-text-centered">
+	    <div class="mt-1 mb-1 pt-1 pb-1 block column is-one-third has-text-centered">
 		    <span class="is-size-6"> {barcode} </span>
 	    </div>
-	    <div class="is-centered is-narrow">
-		    <button class="button" on:click={requestScan}>
+	    <div class="is-narrow">
+		    <button class="button is-fullwidth" on:click={requestScan}>
 			    {#if barcode === null}
 				    Scan Barcode
 			    {:else}
