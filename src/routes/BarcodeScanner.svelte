75d6264ebb2d34537c459a05515535e74a87fe68
kapsiak <kapsi002@umn.edu>
2024-10-17 20:54:27 -0500

    More fixes

--- src/routes/BarcodeScanner.svelte
+++ src/routes/BarcodeScanner.svelte
@@ -1,160 +1,155 @@
 <script lang="ts">
- import { BrowserMultiFormatReader, BarcodeFormat, NotFoundException } from '@zxing/library';
- import { createEventDispatcher, onMount, tick } from 'svelte';
+    import { BrowserMultiFormatReader, BarcodeFormat, NotFoundException } from '@zxing/library';
+    import { createEventDispatcher, onMount, tick } from 'svelte';
 
- async function makeVideoDevice(device_id) {
-	 const video_constraints = { deviceId: { exact: device_id } };
-	 const constraints = { video: video_constraints };
-	 const stream = await navigator.mediaDevices.getUserMedia(constraints);
-	 const [track] = await stream.getVideoTracks();
-	 return [stream, track];
- }
+    let dispatcher = createEventDispatcher();
+    let scan_type = '';
+    const code_reader = new BrowserMultiFormatReader();
 
+    let stream = null;
+    let track = null;
 
- let dispatcher = createEventDispatcher();
- let scan_type = '';
- const code_reader = new BrowserMultiFormatReader();
+    let selected_device_id = null;
 
- let video_device = null;
- let stream = null;
- let track = null;
-
- let promise;
- let decode_promise = null;
- let selected_device_id = null;
- let device_id=null;
-
- let no_cameras=false;
- let all_devices= null;
-
- async function onSelectDevice() {
-     device_id = selected_device_id;
-     startDecode();
- }
-
- async function getDevices() {
-     console.log("HERE2");
-     try{
-         await navigator.mediaDevices.getUserMedia({video: true, audio: false});
-         console.log("HERE2");
-	     var devices = await code_reader.listVideoInputDevices();
-         devices.sort((a,b) => b.label.includes("Back"));
-     } catch (err){
-         var devices = [];
-     }
-     no_cameras = devices.length === 0;
-	 return devices;
- }
-
- let video_element= null;
-
- async function startDecode() {
-	 await stop();
-     console.log(video_element);
-	 [stream, track] = await makeVideoDevice(device_id);
-     await tick();
-	 const p = new Promise((resolve,reject) => code_reader.decodeFromStream(stream, 'video', (result, err) => {
-	     if (result) {
-		     console.log(result);
-		     resolve(result);
-	     }
-	     if (err && !(err instanceof NotFoundException)) {
-		     console.log(err)
-		     resolve(null)
-	 }}));
-     const code = await p;
-     stop();
-     console.log(code);
-	 dispatcher('barcode_scanned', { request: scan_type, value: code });
- }
-
- export function stop() {
-     console.log("Stopping")
-	 if (stream !== null) {
-	     stream.getTracks().forEach((track) => track.stop());
-         track.stop();
-         stream = null;
-         track = null;
-	 }
-	 code_reader.reset();
- }
-
- export async function start(request) {
-     console.log("HERE");
-     if(all_devices === null){
-	     all_devices = await getDevices();
-     }
-     console.log(all_devices);
-
-	 console.log(`Starting scan with request ${request}`);
-     if(no_cameras){
-         console.log("No cameras");
-         return;
-     }
-	 if (isRunning()) stop();
-	 scan_type = request;
-     if (device_id === null ){
-         device_id = all_devices[0].deviceId;
-         //selected_device_id = all_devices[0].deviceId;
-     }
-	 startDecode();
- }
+    let all_devices= null;
+    let video_element= null;
 
 
- $: isRunning = () =>{
-     return (stream !== null);
- }
+    $:  hasCamera = ()=> {
+        return all_devices === null || all_devices.length > 0 ;
+    }
 
- export async function toggle(){
-     if (isRunning()){
-         await stop();
-     }
-     else{
-         await start(scan_type);
-     }
-     
- }
+
+    async function getAllDevices() {
+        try{
+            await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+	        var devices = await code_reader.listVideoInputDevices();
+            devices.sort((a,b) => b.label.includes("Back"));
+        } catch (err){
+            var devices = [];
+        }
+	    return devices;
+    }
+
+    async function makeVideoDevice(device_id) {
+        console.log(device_id);
+	    const video_constraints = { deviceId: { exact: device_id } };
+	    const constraints = { video: video_constraints };
+	    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+	    const [track] = await stream.getVideoTracks();
+	    return [stream, track];
+    }
+
+
+    async function startDecode() {
+	    await stop();
+
+	    [stream, track] = await makeVideoDevice(selected_device_id);
+
+        await tick();
+
+	    const p = new Promise((resolve,reject) => code_reader.decodeFromStream(stream, 'video', (result, err) => {
+	        if (result) {
+		        console.log(result);
+		        resolve(result);
+	        }
+	        if (err && !(err instanceof NotFoundException)) {
+		        console.log(err)
+		        resolve(null)
+	    }}));
+
+        const code = await p;
+        await stop();
+
+	    dispatcher('barcode_scanned', { request: scan_type, value: code });
+    }
+
+    async function onSelectDevice() {
+        startDecode();
+    }
+
+    export function stop() {
+	    if (stream !== null) {
+	        stream.getTracks().forEach((track) => track.stop());
+            track.stop();
+            stream = null;
+            track = null;
+	    }
+	    code_reader.reset();
+    }
+
+    export async function start(request) {
+        console.log("HERE");
+        if(all_devices === null){
+	        all_devices = await getAllDevices();
+        }
+        console.log(all_devices);
+	    console.log(`Starting scan with request ${request}`);
+        if(!hasCamera()) return;
+	    if (isRunning()) stop();
+        if (selected_device_id === null){
+            selected_device_id = all_devices[0].deviceId;
+        }
+	    scan_type = request;
+	    startDecode();
+    }
+
+
+    $: isRunning = () =>{
+        return (stream !== null);
+    }
+
+    export async function toggle(){
+        if (isRunning()){
+            await stop();
+        }
+        else{
+            await start(scan_type);
+        }
+        
+    }
+
+    onMount(async ()=>{
+        all_devices = await getAllDevices();
+        await stop();
+    });
 
 
 </script>
 
-{#if no_cameras }
+{#if !hasCamera() }
     <div class="notification is-danger is-centered">
         Could not find any usable cameras.
     </div>
 {/if}
-{#if isRunning() }
+{#if isRunning()  }
+    <div class="columns is-centered">
+	    <div class="column is-two-thirds">
+	        <video bind:this={video_element} class="" id="video" style="border: 1px solid gray"></video>
+	    </div>
 
-	<div>
-	    <video bind:this={video_element} id="video" width="300" height="200" style="border: 1px solid gray"></video>
-	</div>
+	    <div class="column columns is-full-width">
+            {#if all_devices !== null}
+                <div class="column">
+                    <div class="select">
+                        <select bind:value={selected_device_id} on:change={onSelectDevice}>
+		                    {#each all_devices as device}
+			                    <option value={device.id}>
+			                        {device.label}
+			                    </option>
+		                    {/each}
+	                    </select>
+                    </div>
+                </div>
+	        {/if}
+	        {#if selected_device_id !== null}
+                <div class="is-narrow">
+	                <button class="button" on:click={toggle}> {(isRunning())? "Stop" : "Start"} Decode</button>
+                </div>
+            {/if}
+	    </div>
 
-    <div class="select">
-        <select bind:value={selected_device_id} on:change={onSelectDevice}>
-	        {#await promise}
-		        <option value={null}> Loading Available Video Devices... </option>
-	        {:then devices}
-		        {#if devices.length > 0}
-		            {#each devices as device}
-			            <option value={device.id}>
-			                {device.label}
-			            </option>
-		            {/each}
-		        {/if}
-			{:catch someError}
-		        System error: {someError.message}.
-	        {/await}
-	    </select>
     </div>
-	{#if device_id !== null}
-	    <button on:click={toggle}> {(isRunning())? "Stop" : "Start"} Decode</button>
-	{/if}
-
-	{#await decode_promise}
-	    Please Scan Barcode
-	{:catch someError}
-	    System error: {someError.message}.
-	{/await}
 {/if}
 <!-- <div>  Is Running {isRunning()}
      </div> -->
